124a7d467437b58d4381da9d77ceef7307385713
harry <harry.wang0210@gmail.com>
2023-05-29 11:00:05 +0800

    unit selection widget update 20230529

--- UnitSelection.py
+++ UnitSelection.py
@@ -1,8 +1,9 @@
-from UI.Unit_selectionUIv2_ui import Ui_Form
+from UI.Unit_selectionUIv2_ui import Ui_UnitOperateTools
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class UnitSelection(QtWidgets.QWidget, Ui_Form):
+class UnitSelection(QtWidgets.QWidget, Ui_UnitOperateTools):
     def __init__(self, parent=None):
         super().__init__(parent)
+
         self.setupUi(self)
