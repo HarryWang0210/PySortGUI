932da3ff8b1d24b328da2d9a3b471df2a2bab2cd
harry <harry.wang0210@gmail.com>
2023-08-05 16:08:39 +0800

    receive data

--- Widgets/TimelineView.py
+++ Widgets/TimelineView.py
@@ -6,9 +6,13 @@ from PyQt5.QtWidgets import QOpenGLWidget
 from PyQt5.QtCore import Qt
 from OpenGL.GL import *
 import numpy as np
-import h5py
+from DataStructure.data import SpikeSorterData
+
 
 class TimelineView(QtWidgets.QWidget, Ui_TimelineView):
+    signal_data_file_name_changed = QtCore.pyqtSignal(str)
+    signal_spike_chan_changed = QtCore.pyqtSignal(object)
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.window_title = "Timeline View"
@@ -16,16 +20,28 @@ class TimelineView(QtWidgets.QWidget, Ui_TimelineView):
         self.openGLWidget = TimelineView_widget(self)
         self.openglLayout.addWidget(self.openGLWidget)
 
+    def spike_chan_changed(self, data):
+        self.openGLWidget.get_info(data)
+        self.openGLWidget.raw_visible = True
+        self.openGLWidget.update()
+        # self.opengl_widget.makeCurrent()
+        # self.opengl_widget.paintGL()
+        # self.opengl_widget.swapBuffers()
+
+
 class TimelineView_widget(QOpenGLWidget):
+
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.window_title = "Timeline View"
         self.setMinimumWidth(100)
         self.setMinimumHeight(100)
+        self.raw_visible = False
 
-        filename = "data/MX6-22_2020-06-17_17-07-48_no_ref.h5"
-        f = h5py.File(filename, "r")
-        self.data = f["Raws/raw000"][()]
+    def initializeGL(self):
+        glClearColor(0.35, 0.35, 0.35, 0.8)  # background color
+
+    def get_info(self, data):
+        self.data = data
         # initial scale data between -1 and 1 to fit the window
         self.data_scale = np.median(np.abs(self.data)) * 10
 
@@ -36,73 +52,29 @@ class TimelineView_widget(QOpenGLWidget):
         self.scale_x = 1.0
         self.scale_y = 1.0
 
-
-    def initializeGL(self):
-        glClearColor(0.35, 0.35, 0.35, 0.8)  # background color
-        # vertex_shader = QOpenGLShader(QOpenGLShader.Vertex, self)
-        # vertex_shader.compileSourceCode("""
-        #     attribute vec2 position;
-        #     void main() {
-        #         gl_Position = vec4(position, 0.0, 1.0);
-        #     }
-        # """)
-        # fragment_shader = QOpenGLShader(QOpenGLShader.Fragment, self)
-        # fragment_shader.compileSourceCode("""
-        #     uniform vec3 color;
-        #     void main() {
-        #         gl_FragColor = vec4(color, 1.0);
-        #     }
-        # """)
-        # self.shader_program = QOpenGLShaderProgram(self)
-        # self.shader_program.addShader(vertex_shader)
-        # self.shader_program.addShader(fragment_shader)
-        # self.shader_program.link()
-
-    # def resizeGL(self, width, height):
-    #     glViewport(0, 0, width, height)
-    #     glMatrixMode(GL_PROJECTION)
-    #     glLoadIdentity()
-    #     glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
-    #     glMatrixMode(GL_MODELVIEW)
-
     def paintGL(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        # self.shader_program.bind()
+        if self.raw_visible:
 
-        # glPushMatrix()
+            glColor3f(1.0, 1.0, 1.0)
+            data_scale = self.data_scale / self.scale_y
+            num_data_show = int(self.num_data_show / self.scale_x)
+            line_strip_data = []
+            for i, y in enumerate(self.data[self.offset: self.offset + num_data_show + 1]):
+                x = -1.0 + i * 2 / num_data_show
 
-        # glScale(1 * self.scale_x, 1.0, 1.0)
+                line_strip_data.extend([x, y / data_scale])
+            self.draw_line_strip(line_strip_data)
+            # glPopMatrix()
+            glViewport(0, 0, self.width(), self.height())
 
-        # line_strip_data = []
-        # for i, y in enumerate(self.data[self.offset:self.offset+10]):
-        #     x = -1.0 + i * 0.2
-        #     line_strip_data.extend([x, y])
-        # self.draw_line_strip(line_strip_data)
+            # threshold
+            thr = self.thr / data_scale
+            glColor3f(0.0, 1.0, 0.0)
+            # self.shader_program.setUniformValue(color_location, 0.0, 1.0, 0.0)
+            self.draw_line_strip([-1.0, thr, 1.0, thr], True)
 
-        # glPopMatrix()
-
-        # color_location = self.shader_program.uniformLocation('color')
-        # self.shader_program.setUniformValue(
-        #     color_location, 1.0, 1.0, 1.0)  # line color
-        glColor3f(1.0, 1.0, 1.0)
-        data_scale = self.data_scale / self.scale_y
-        num_data_show = int(self.num_data_show / self.scale_x)
-        line_strip_data = []
-        for i, y in enumerate(self.data[self.offset: self.offset + num_data_show + 1]):
-            x = -1.0 + i * 2 / num_data_show
-
-            line_strip_data.extend([x, y / data_scale])
-        self.draw_line_strip(line_strip_data)
-        # glPopMatrix()
-        glViewport(0, 0, self.width(), self.height())
-
-        # threshold
-        thr = self.thr / data_scale
-        glColor3f(0.0, 1.0, 0.0)
-        # self.shader_program.setUniformValue(color_location, 0.0, 1.0, 0.0)
-        self.draw_line_strip([-1.0, thr, 1.0, thr], True)
-
-        # self.shader_program.release()
+            # self.shader_program.release()
 
     def draw_line_strip(self, data, is_line=False):
         # 畫出範圍內的圖
