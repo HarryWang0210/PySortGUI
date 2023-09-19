aa0770a4fee9033665896f48347ae8a2a3fb988a
harry <harry.wang0210@gmail.com>
2023-09-19 13:00:23 +0800

    manage value type, add waveforms_pca

--- DataStructure/data.py
+++ DataStructure/data.py
@@ -1,77 +1,12 @@
-# import logging
-# import traceback
-# from importlib import import_module
-# import pkgutil
-
-# from functools import wraps
-
-# _MODULES_NAME = ['openephys']
-# _MODULES_DICT = {}
-# _HEADER_FUNC_DICT = {}
-# logger = logging.getLogger(__name__)
-
-# for module in _MODULES_NAME:
-#     # load input modules and save to  _MODULES_DICT
-#     try:
-#         prefix = __name__.rsplit('.', 1)[0] + '.'
-#         _MODULES_DICT[module] = import_module(prefix + module)
-#     except:
-#         logger.error('Not able to import module {}'.format(module))
-
-# def _execution_error(func):
-#     '''
-#     A wrapper that returns error if execution fails without raising an excetpion.
-#     This decorator switches an exception with a logger.error
-#     :param func: function
-#     '''
-
-#     @wraps(func)
-#     def decorate(*args, **kws):
-#         try:
-#             return func(*args, **kws)
-#         except Exception as _:
-#             #exec_info = sys.exc_info()
-#             logger.error('\t ******Execution halted for {}******\n{}'.format(
-#                 func.__name__, traceback.format_exc()))
-
-#     return decorate
-
-# @_execution_error
-# def _export_to_pyephys_single_file(file_name):
-#     '''
-
-#     :param file_name:
-#     :type file_name:
-#     '''
-
-#     msg = "exporting {}".format(file_name)
-#     logger.info(msg)
-
-#     header = read_header(file_name)
-#     data = read_data(file_name)
-
-#     if header is None or header is None:
-#         return
-
-#     lib_pe.create_pyephys_datafile(header, data)
-
-# # this decorator raises an exception if the given filename is not a file
-# @_check_file_exist # 先確認file是存在再呼叫 read_header
-# def read_header(file_name):
-
-#     _, ext = os.path.splitext(file_name) # 分開檔案名, 擴展名
-#     if ext.lower() in _HEADER_FUNC_DICT:
-#         return _HEADER_FUNC_DICT[ext](file_name) # 使用該擴展名read header的function
-#     else:
-#         logger.warn('File extension not known or not implemented yet')
-
-
 import tables
 import os
 import pandas as pd
 import numpy as np
 from DataStructure.pyephys import load_pyephys, load_raws, load_spikes
 # from pyephys import load_pyephys, load_raws
+
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import MaxAbsScaler
 
 
 class SpikeSorterData():
@@ -86,8 +21,15 @@ class SpikeSorterData():
         self.__chan_info = self.__chan_info.reindex(columns=column_order)
         self.__chan_info.set_index(['ID', 'Label'], inplace=True)
         self.__chan_info.sort_index(inplace=True)
+
+        # turn id to string
+        self.__chan_info.index = self.__chan_info.index.set_levels(
+            self.__chan_info.index.levels[0].astype('string'), level="ID")
+        self.__chan_info["ReferenceID"] = self.__chan_info["ReferenceID"].astype(
+            'string')
+
         self.__raw_data = dict()
-        self.__unit_data = dict()
+        self.__spike_data = dict()
 
     @property
     def chan_info(self):
@@ -101,8 +43,8 @@ class SpikeSorterData():
 
         # round float
         chan_info["Threshold"] = chan_info["Threshold"].round(3)
-
-        chan_info = chan_info.replace([np.NaN], [None])
+        # chan_info = chan_info.replace([np.NaN], [None])
+        # print(chan_info.index[0][0], type(chan_info.index[0][0]))
         return chan_info
 
     def get_raw(self, chan_ID):
@@ -112,10 +54,21 @@ class SpikeSorterData():
         return self.__raw_data[chan_ID]
 
     def get_spikes(self, chan_ID, label):
-        if chan_ID not in self.__unit_data.keys():
-            self.__unit_data[chan_ID] = load_spikes(
+        if chan_ID not in self.__spike_data.keys():
+            self.__spike_data[chan_ID] = load_spikes(
                 self.filename, chan_ID, label)
-        return self.__unit_data[chan_ID]
+        return self.__spike_data[chan_ID]
+
+    def wavforms_pca(self, chan_ID, label):
+        data = self.get_spikes(chan_ID, label)['waveforms']
+        chan_ID = int(chan_ID)
+        pca = PCA(n_components=3)
+        pca.fit(data)
+        transformed_data = pca.transform(data)
+
+        transformer = MaxAbsScaler().fit(transformed_data)  # fit does nothing.
+        transformed_data = transformer.transform(transformed_data)
+        return transformed_data
 
 
 if __name__ == '__main__':
