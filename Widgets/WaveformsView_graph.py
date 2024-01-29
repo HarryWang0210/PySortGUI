e219b2dc9b8874c934953efae5644a5a8cf52832
harry <harry.wang0210@gmail.com>
2024-01-30 01:34:06 +0800

    refactor: datav3 compatibility

--- Widgets/WaveformsView_graph.py
+++ Widgets/WaveformsView_graph.py
@@ -6,8 +6,8 @@ import pyqtgraph as pg
 import numpy as np
 import seaborn as sns
 import time
-from DataStructure.data import SpikeSorterData
 from Widgets.WidgetsInterface import WidgetsInterface
+from DataStructure.datav3 import SpikeSorterData, ContinuousData, DiscreteData
 
 import logging
 logger = logging.getLogger(__name__)
@@ -22,21 +22,22 @@ class WaveformsView(pg.PlotWidget, WidgetsInterface):
 
         self.data_object = None  # SpikeSorterData object
         self.data_scale = 1.0
-        self.spikes = None
-        self.has_spikes = False
+        # self.spikes = None
+        # self.has_spikes = False
         self.thr = 0.0
         self.has_thr = False
         self.color_palette_list = sns.color_palette('bright', 64)
         self.visible = False  # overall visible
 
-        self.num_unit = 1
-        self.current_wav_units = []
+        # self.num_unit = 1
+        # self.current_wav_units = []
         self.current_wavs_mask = []
-        self.current_wav_colors = []
+        # self.current_wav_colors = []
 
         self.current_showing_units = []
-        self.current_showing_data = []
+        # self.current_showing_data = []
 
+        self.current_spike_object = None
         self.manual_mode = False
 
         self.initPlotItem()
@@ -88,10 +89,21 @@ class WaveformsView(pg.PlotWidget, WidgetsInterface):
         self.visible = False
         self.updatePlot()
 
-    def spike_chan_changed(self, current_chan_info):
-        self.getThreshold(current_chan_info["Threshold"])
-        # self.getSpikes(current_chan_info["ID"], current_chan_info["Label"])
-        self.getSpikes(current_chan_info)
+    def showing_spike_data_changed(self, new_spike_object: DiscreteData | None):
+        self.current_spike_object = new_spike_object
+
+        # self.has_spikes = True
+        self.visible = True
+        if self.current_spike_object is None:
+            # self.has_spikes = False
+            self.visible = False
+            return
+        self.data_scale = np.max(np.abs(self.current_spike_object.waveforms))
+
+    # def spike_chan_changed(self, current_chan_info):
+    #     self.getThreshold(current_chan_info["Threshold"])
+    #     # self.getSpikes(current_chan_info["ID"], current_chan_info["Label"])
+    #     self.getSpikes(current_chan_info)
 
     # def extract_wav_changed(self, wav_dict):
     #     self.has_spikes = True
@@ -119,17 +131,23 @@ class WaveformsView(pg.PlotWidget, WidgetsInterface):
     #     self.redraw = False
     #     self.updatePlot()
 
-    def showing_spikes_data_changed(self, spikes_data):
-        if self.has_spikes:
-            self.current_wav_units = spikes_data['current_wav_units']
-            self.current_wavs_mask = np.isin(spikes_data['current_wav_units'],
-                                             spikes_data['current_showing_units'])
-            self.current_showing_units = spikes_data['current_showing_units']
-            self.num_unit = len(np.unique(self.current_wav_units))
-
-            self.current_wav_colors = self.getColor(self.current_wav_units)
-            self.setCurrentShowingData()
+    def showing_units_changed(self, showing_unit_IDs):
+        self.current_showing_units = showing_unit_IDs
+        self.current_wavs_mask = np.isin(self.current_spike_object.unit_IDs,
+                                         self.current_showing_units)
         self.updatePlot()
+
+    # def showing_spikes_data_changed(self, spikes_data):
+    #     if self.has_spikes:
+    #         self.current_wav_units = spikes_data['current_wav_units']
+    #         self.current_wavs_mask = np.isin(spikes_data['current_wav_units'],
+    #                                          spikes_data['current_showing_units'])
+    #         self.current_showing_units = spikes_data['current_showing_units']
+    #         # self.num_unit = len(np.unique(self.current_wav_units))
+
+    #         self.current_wav_colors = self.getColor(self.current_wav_units)
+    #         self.setCurrentShowingData()
+    #     self.updatePlot()
 
     def activate_manual_mode(self, state):
         self.manual_mode = state
@@ -137,70 +155,73 @@ class WaveformsView(pg.PlotWidget, WidgetsInterface):
     def select_point(self, data):
         selected, wav_index = data
         if selected:
-            y = self.current_showing_data[wav_index, :]
+            y = self.current_spike_object.waveforms[wav_index, :]
             x = np.arange(len(y))
             self.select_point_item.setData(x, y)
 
         self.select_point_item.setVisible(selected)
 
-    def getThreshold(self, thr):
-        self.thr = float(thr)
-        if np.isnan(self.thr):
-            self.has_thr = False
-        else:
-            self.has_thr = True
+    # def getThreshold(self, thr):
+    #     self.thr = float(thr)
+    #     if np.isnan(self.thr):
+    #         self.has_thr = False
+    #     else:
+    #         self.has_thr = True
 
-    def getSpikes(self, current_chan_info):
-        # def getSpikes(self, chan_ID, label):
-        spikes = current_chan_info
+    # def getSpikes(self, current_chan_info):
+    #     # def getSpikes(self, chan_ID, label):
+    #     spikes = current_chan_info
 
-        # spikes = self.data_object.getSpikes(chan_ID, label)
-        if spikes["unitID"] is None:
-            self.has_spikes = False
-            self.spikes = None
-            self.visible = False
+    #     # spikes = self.data_object.getSpikes(chan_ID, label)
+    #     if spikes["unitID"] is None:
+    #         self.has_spikes = False
+    #         self.spikes = None
+    #         self.visible = False
 
-            self.data_scale = 1.0
-        else:
-            self.has_spikes = True
-            self.spikes = spikes
-            self.visible = True
+    #         self.data_scale = 1.0
+    #     else:
+    #         self.has_spikes = True
+    #         self.spikes = spikes
+    #         self.visible = True
 
-            self.data_scale = np.max(np.abs(spikes["waveforms"]))
-        logger.debug(self.visible)
+    #         self.data_scale = np.max(np.abs(spikes["waveforms"]))
+    #     logger.debug(self.visible)
 
-    def getColor(self, unit_data):
-        n = len(unit_data)
-        color = np.zeros((n, 3))
+    # def getColor(self, unit_data):
+    #     n = len(unit_data)
+    #     color = np.zeros((n, 3))
 
-        for i in range(n):
-            color[i, :] = self.color_palette_list[int(unit_data[i])]
-        color = color * 255
-        return color.astype(np.int32)
+    #     for i in range(n):
+    #         color[i, :] = self.color_palette_list[int(unit_data[i])]
+    #     color = color * 255
+    #     return color.astype(np.int32)
 
-    def setCurrentShowingData(self):
-        if not self.has_spikes:
-            return
-        self.current_showing_data = self.spikes['waveforms'][self.current_wavs_mask]
+    # def setCurrentShowingData(self):
+    #     if self.current_spike_object is None:
+    #         return
+    #     self.current_showing_data = self.spikes['waveforms'][self.current_wavs_mask]
 
     def updatePlot(self):
-        if self.visible and self.has_spikes:
-            self.drawWaveforms(self.current_showing_data,
-                               self.current_showing_units)
-            if self.has_thr:
-                self.drawThreshold()
+        if self.visible and not self.current_spike_object is None:
+            self.removeWaveformItems()
+
+            self.drawWaveforms()
+            # if self.has_thr:
+            self.drawThreshold()
 
         for waveforms_item in self.waveforms_item_list:
-            waveforms_item.setVisible(self.visible and self.has_spikes)
+            waveforms_item.setVisible(
+                self.visible and not self.current_spike_object is None)
 
-        self.thr_item.setVisible(self.visible and self.has_thr)
+        self.thr_item.setVisible(self.visible)
 
     def drawThreshold(self):
-        self.thr_item.setValue(self.thr)
+        self.thr_item.setValue(self.current_spike_object.threshold)
 
-    def drawWaveforms(self, waveforms, unit_ID):
-        self.removeWaveformItems()
+    def drawWaveforms(self):
         # create elements
+        waveforms = self.current_spike_object.waveforms[self.current_wavs_mask]
+        unit_IDs = self.current_spike_object.unit_IDs[self.current_wavs_mask]
         xlen = waveforms.shape[1]
         x_element = np.arange(xlen)
         connect_element = np.append(np.ones(xlen - 1), 0).astype(np.int32)
@@ -210,9 +231,8 @@ class WaveformsView(pg.PlotWidget, WidgetsInterface):
             x_element[0], x_element[-1], padding=0)
         self.plot_item.getViewBox().setYRange(-self.data_scale, self.data_scale, padding=0)
 
-        for ID in unit_ID:
-            ID_mask = self.current_wav_units[self.current_wavs_mask] == ID
-            data_filtered = waveforms[ID_mask]
+        for ID in self.current_showing_units:
+            data_filtered = waveforms[unit_IDs == ID]
             n = data_filtered.shape[0]
 
             if n == 0:
@@ -222,7 +242,8 @@ class WaveformsView(pg.PlotWidget, WidgetsInterface):
             y = np.ravel(data_filtered)
             connect = np.tile(connect_element, n)
 
-            color = self.current_wav_colors[self.current_wav_units == ID][0, :]
+            color = self.color_palette_list[int(ID)]
+            color = (np.array(color) * 255).astype(int)
             pen = pg.mkPen(
                 color=color)
 
