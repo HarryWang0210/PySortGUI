a7c46dc10e72d48891d205a77d70301bb779d1b9
harry <harry.wang0210@gmail.com>
2024-05-26 23:20:59 +0800

    feat: Add widgetVisibilityChanged abstract slot

--- Widgets/WidgetsInterface.py
+++ Widgets/WidgetsInterface.py
@@ -1,4 +1,8 @@
+import logging
+
 from PyQt5.QtWidgets import QWidget
+
+logger = logging.getLogger(__name__)
 
 
 class WidgetsInterface(QWidget):
@@ -7,3 +11,14 @@ class WidgetsInterface(QWidget):
         self.window_title = "Widgets Interface"
         self.setMinimumWidth(100)
         self.setMinimumHeight(100)
+        # self.widget_active: bool = False
+
+    def widgetVisibilityChanged(self, visible: bool):
+        """Abstract Slot: detect whether widget is visible
+
+        Args:
+            visible (bool): _description_
+        """
+        pass
+        # self.widget_active = visible
+        # logger.debug(f'{self.window_title}: {self.widget_active}')
