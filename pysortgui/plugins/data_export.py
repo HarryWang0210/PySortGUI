da372136eea9e1660fb575cf02f9f13a0c8d1100
harry <harry.wang0210@gmail.com>
2024-08-06 16:37:43 +0800

    refactor: Change the command args

--- pysortgui/plugins/data_export.py
+++ pysortgui/plugins/data_export.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-
-'''
-Created on May 4, 2017
-
-@author: scaglionea
-'''
-
+# modified form pyephys.plugins.data_utils.py
+import logging
 import os
 
 import click
@@ -18,6 +12,7 @@ from pysortgui.plugins import IPlugin
 # import pprint
 
 # import pdb
+logger = logging.getLogger(__name__)
 
 # PLUGIN -----------------------------------------------------------------
 
@@ -28,19 +23,23 @@ class DataConvert(IPlugin):
         @cli.group()
         def data():
             '''
-            Data info and import group :pyephys:
+            Data convert group
             '''
             pass
         # import command
 
         @data.command('convert')
         @click.argument('paths', type=click.Path(exists=True, file_okay=True, dir_okay=True), nargs=-1)
-        @click.argument('input_format', nargs=1)
-        @click.argument('output_format', nargs=1)
+        @click.option('-i', '--input_format', 'input_format', type=str, nargs=1,
+                      default='openephys', show_default=True,
+                      help='Input file/folder format')
+        @click.option('-o', '--output_format', 'output_format', type=str, nargs=1,
+                      default='pyephys', show_default=True,
+                      help='Output file/folder format')
         @click.help_option('-h', '--help')
         @click.pass_context
         def convert_files(ctx, paths, input_format, output_format):
-            """Convert the file or folder into output_format. 
+            """Convert the file or folder from input_format to output_format.
             Can convert multiple files."""
             if len(paths) == 0:
                 paths = '.'
@@ -48,10 +47,10 @@ class DataConvert(IPlugin):
             for path in paths:
                 data_object = SpikeSorterData(file_or_folder=path,
                                               data_format=input_format)
-                file = data_object.path
+
+                new_filename = data_object.path
                 if output_format == 'pyephys':
-                    new_filename = os.path.splitext(file)[0] + '.h5'
-                    data_object.export(new_filename=new_filename,
-                                       data_format=output_format)
-                else:
-                    pass
+                    new_filename = os.path.splitext(new_filename)[0] + '.h5'
+
+                data_object.export(new_filename=new_filename,
+                                   data_format=output_format)
