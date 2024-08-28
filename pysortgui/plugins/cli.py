4a9fb428d2fd08b213a4877693b03c4d0cdc67d8
harry <harry.wang0210@gmail.com>
2024-08-28 14:56:58 +0800

    feat: Enable debug logger option

--- pysortgui/plugins/cli.py
+++ pysortgui/plugins/cli.py
@@ -1,22 +1,29 @@
+import logging
+
 import click
 from pysortgui.plugins.data_export import DataConvert
+
+logger = logging.getLogger(__name__)
 
 
 @click.group()
 # @click.version_option(version=__version_git__)
+@click.option('-d', '--debug', is_flag=True, default=False)
 @click.help_option('-h', '--help')
 @click.pass_context
-def pysortgui_cli(ctx, pdb=None, ipython=None, prof=None, lprof=None):
+def pysortgui_cli(ctx, debug):
     """
     pysortgui command line interface.
 
     \b
     For help on entries listed below:
     >>>pysortgui-cli `entry` --help"""
-    pass
+    if debug:
+        root_logger = logging.getLogger('pysortgui')
+        for handler in root_logger.handlers:
+            handler.setLevel(logging.DEBUG)
+        logger.debug("Enable debug logger")
 
-
-# @click.command()
 # @click.option('-t', '--to', 'to', help='To who')
 # def greeting(to):
 #     '''Say hello to someone'''
