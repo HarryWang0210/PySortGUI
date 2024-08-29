eb712ddc9fceb11b5f3ccd76e10ac9a72dcc5cca
harry <harry.wang0210@gmail.com>
2024-08-29 14:14:18 +0800

    fix: Disable unimplemented actions, wait for future update

--- pysortgui/MainWindowDocks.py
+++ pysortgui/MainWindowDocks.py
@@ -98,7 +98,7 @@ class MainWindowDocks(QtWidgets.QMainWindow):
         SaveAllAction.setStatusTip('Save all channels')
         FileMenu.addAction(SaveAllAction)
         FileMenu_dict["SaveAll"] = SaveAllAction
-        # SaveAllAction.setEnabled(False)  # Unimplemented function
+        SaveAllAction.setEnabled(False)  # Unimplemented function
 
         ExportAction = QtWidgets.QAction('Export', self)
         # ExportAction.setShortcut('Ctrl+Alt+S')
@@ -159,7 +159,7 @@ class MainWindowDocks(QtWidgets.QMainWindow):
         RenameSpikeAction = QtWidgets.QAction('Rename this Spike Data', self)
         EditMenu.addAction(RenameSpikeAction)
         EditMenu_dict["RenameSpike"] = RenameSpikeAction
-        # RenameSpikeAction.setEnabled(False)  # Unimplemented function
+        RenameSpikeAction.setEnabled(False)  # Unimplemented function
 
         self.children_dict["EditMenu"] = EditMenu_dict
 
